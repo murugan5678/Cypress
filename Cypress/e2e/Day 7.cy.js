b1cdbc96289899d793ec217a68ba511e363bd449
murugan5678 <murugan@qaoncloud.com>
2024-12-12 13:39:36 +0530

    updated code

---
+++ Cypress/e2e/Day 7.cy.js
@@ -0,0 +1,32 @@
+describe('Day7hooks', ()=>{
+
+    before('title check', ()=>{
+     cy.visit("https://testautomationpractice.blogspot.com/")
+     cy.title().then((title)=>{
+         cy.log(title);
+     })
+ })
+ after('code end time', ()=>{
+     cy.log(new Date().toLocaleTimeString())
+ })
+ afterEach('screenshot',function(){
+     cy.screenshot('test-screenshot',{capture: 'fullPage' });
+    }) 
+
+    beforeEach('clear text', ()=>{
+     cy.visit("https://testautomationpractice.blogspot.com/")
+     cy.xpath("//input[@id='field1']").clear()
+ })
+ it('enter text hook', ()=>{
+     //cy.visit("https://testautomationpractice.blogspot.com/")
+     cy.xpath("//input[@id='field1']").type('hook')
+     cy.get("button[ondblclick='myFunction1()']").dblclick()
+
+ })
+ it('enter text HOOK', ()=>{
+     //cy.visit("https://testautomationpractice.blogspot.com/")
+     cy.xpath("//input[@id='field1']").type('HOOK')
+     cy.get("button[ondblclick='myFunction1()']").dblclick()
+
+ })
+})
