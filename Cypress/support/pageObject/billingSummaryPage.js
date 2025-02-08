df25fd292cb374037e0464d5cbc374c082622868
murugan5678 <murugan@qaoncloud.com>
2025-02-08 13:37:18 +0530

    Delete Cypress/support/pageObject/billingSummaryPage.js

--- Cypress/support/pageObject/billingSummaryPage.js
+++
@@ -1,9 +0,0 @@
-class billingSumPage {
-    billingSum() {
-        
-        cy.wait(5000);
-    
-    cy.screenshot();
-    cy.xpath("(//button[@id='btnNext'])[1]").click();
-    }}
-    export default new billingSumPage();
